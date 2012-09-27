5a5d263cfc3f00dd4f44f946ef3f360cbdc98f72
Carlos Martin <carlos@3xlogic.com>
2012-09-27 16:14:16 -0600

    Updated Version

---
+++ README.md
@@ -0,0 +1,76 @@
+Getting started with 3xCLOUD Plugin
+
+Link files
+Add these items to the <head> of your document. This will link jQuery and the 3xCLOUD plugin into your webpage.
+<!-- Place somewhere in the <head> of your document -->
+<script src=" https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js "></script>
+<script src="https://3xcloud.net/webclient/repo/jquery.3xlogic.0.1.3.js"></script>
+
+Add Markup
+The 3xCLOUD markup is simple and straightforward. All the cameras associated with the API Key will be contain in a div element (<div id=’camera1’></div>) with a unique id identifier, in this case the id is “camera1”. Because we associated two cameras to the API Key, this example will add two div elements to the body on the page.
+<!-- Place somewhere in the <body> of your page -->
+<div id=’camera1’></div>
+<div id=’camera2’></div>
+
+The web developer can customize the div container with any CSS style he or she desires, for this example we are going to configure the div with the ids “camera1 and camera2” to be 300x300.
+<!-- Place somewhere in the <head> of your page -->
+#camera1, #camera2 {
+	width: 300px;
+	height: 300px;
+}
+
+Hook up to 3xCLOUD
+Create a configuration object adding the API Key using a method “apiKey”.
+var config = {
+	apiKey: “MyAPIKey”
+}  
+3xCLOUD plugin associate cameras by number, because we have to cameras associated with the API Key “MyAPIKey” the first camera is identified with the number 1 and the second camera is identified with the number 2. The following example will put all the concepts together.
+<!—Place in the <head>, after the three links and inside $(document).ready() -->
+<script type=”text/javascript”>
+$(document).ready(function(){
+$(“#camera1”)._3xcloud(config, 1);
+$(“#camera2”)._3xcloud(config, 2);
+});
+</script>
+
+Adapt to your needs
+Listed below are all of the options available to customize 3xCLOUD to suite your needs, along with their default values. 
+The configuration Object
+apiKey: “YourAPIKey”
+port: 80
+url: “https://3xcloud.net/webclient”
+
+Example:
+var config = {
+	apiKey: “MyAPIKey”,
+	port: 443,
+	url: “https://3xcloud.net/webclient”
+}  
+3xCLOUD Options
+caption: false  //Display the camera name on the top left corner (Customizable )
+Example:
+$(“#camera1”)._3xcloud(config, 1, { caption: true });
+
+ratio: false   //If set to true, it will keep the aspect ratio of the camera, otherwise is set to 100% width and height.
+Example:
+$(“#camera1”)._3xcloud(config, 1, { ratio: true});
+
+captionStyle: null   //Use to change the looks of the camera’s caption. To use this setting caption must be set to true.
+Example:
+$(“#camera1”)._3xcloud(config, 1, { caption: true, captionStyle: {“color”:”red”} });
+
+captionClass: null  //User to associate a CSS class to the camera’s caption
+Example:
+<style>
+.myClass {
+	Color: #FF0000;
+}
+</style>
+$(“#camera1”)._3xcloud(config, 1, { caption: true, captionClass: “myClass” });
+
+
+Live Demo
+To get familiar with 3xCLOUD plugin you can test and modify the code at http://jsfiddle.net/3xlogic/69kVC/13/
+
+Source Code
+If you want to improve 3xCLOUD to suite the need of your project, you can download the uncompressed version at  
--- jQueryPlugin/jquery.3xlogic.0.1.3.js
+++ jQueryPlugin/jquery.3xlogic.0.1.3.js
@@ -11,15 +11,16 @@
  */
 (function($, window, document, undefined){
 	 
-	 $.fn._3xlogic = function(options, camera, config)
+	 $.fn._3xcloud = function(options, camera, config)
 	 {
-		 camera -=1;
+		 camera -=1; //To allow user to start cameras count from 1 and not 0
 		 
-		 var conf = $.extend({}, $.fn._3xlogic.defaultOptions, config);
+		 var conf = $.extend({}, $.fn._3xcloud.defaultOptions, config);
 		 
-		 var settings = $.extend({
-			 url:'mydvr.com',
-			 port: '5555',
+		 var settings = $.extend({},
+		 {
+			 url:'https://3xcloud.net/webclient',
+			 port: '80',
 			 apiKey: 'apiKeygoeshere',
 			 
 	 	 },options);
@@ -27,139 +28,183 @@
 		 
 		 //Creating an inner container
 		 var container = $("<div>").appendTo(this);
-		 $(container).css({'width':'100%', 'height':'100%','background-color':'black'});
 		 
-		 var base = $(container);
+		 $(container).css(
+		 	{
+				'width':'100%', 
+				'height':'100%',
+				'background-color':'black'
+			});
+		 
+		 //Global version of the general Container
+		 var base = $(container); 
 		 
 		 //Setting caption
-		 var captionSettings = function(){
+		 var captionSettings = function()
+		 {
 		   if(conf.caption)
 		   {
-			      $.ajax({
-					   url: settings.url + "/api/info/",
-					   type:'GET',
-					   data:{ 
+			      $.getJSON(settings.url + "/api/info/?callback=?",
+					   { 
 							  key: settings.apiKey,
 							  cam: camera,
-							  callback:''
 					   },
-					   dataType:"json",
-					   success: function(data)
+					  function(data)
 					   {
-								  //console.log(data[0].name);
-								  var label = $("<label>").text(data[0].name);
-								  
-								  base.css({'position':'relative'});
-								  
-								  $(label).appendTo(base);
-								  
-								  $(label).css({'position':'absolute','top':'0','z-index':'2','left':'0','color':'white'})
-								  .data('info',data[0]);
-								  
-								  if(conf.captionStyle){
-									  $(label).css(conf.captionStyle);
-								  }
-								  
-								  if(conf.captionClass){
-									  $(label).addClass(conf.captionClass);
-								  }
-					   },
-					   error: function(data){
-						   //console.log("Error");
-						   //console.log(data[0]);
-					   }
-				  });
-		   }
+							//Camera Name;
+							var label = $("<label>").text(data[0].name);
+							
+							base.css({'position':'relative'});
+							
+							$(label).appendTo(base);
+							
+							$(label).css(
+							{
+								'position':'absolute',
+								'top':'0',
+								'z-index':'2',
+								'left':'0',
+								'color':'white'
+							})
+							.data('info',data[0]);
+							
+							//Checking user's Configurations
+							if(conf.captionStyle)
+							{
+								$(label).css(conf.captionStyle);
+							}
+							
+							if(conf.captionClass)
+							{
+								$(label).addClass(conf.captionClass);
+							}
+					   });
+		  	}
 		 }
 		 
-		  var getDimenssions = function(){
-			// console.log(base.width() + "x" + base.height());
+		  var getDimenssions = function()
+		  {
+			// Determine the dimenssion of the video container;
 			 return base.width() + "x" + base.height();
 		  }
 		 
-		 var getSrc = function(){
+		 var getSrc = function()
+		 {
 			var rdn = Math.floor(Math.random() * 1000000000000);
-			var src = settings.url + "/api/video/?key="+ settings.apiKey + "&cam=" + camera;
-			src= src + "&resolution=" + getDimenssions() + "&" + rdn;
+		
+			var src = settings.url        + 
+				      "/api/video/?key="  + 
+					  settings.apiKey     + 
+					  "&cam="             +
+					  camera;
+		
+			src += "&resolution="   + 
+				   getDimenssions() + 
+				   "&"              + 
+				   rdn;
+		
 			return src;
 		}
 		
-		
-		 
 		 //Adding video to container
-		 var createImage  = function(){
-			 var video = $("<img src='" + getSrc() + "'>")
-			 			  .data('_3xlogic',{
-							                 url: settings.url + "/api/video/?key="+ settings.apiKey + "&cam=" + camera,
-											 cam: camera
-											 });
-											 
-			 return video;
-		 }
+		 var createImage  = function()
+						   {
+							   var video = $("<img src='" + getSrc() + "'>")
+										  //Saving basic info into the img object	
+										   .data('_3xlogic',
+										   {
+											 url: settings.url 		  + 
+												  "/api/video/?key="  + 
+												  settings.apiKey     + 
+												  "&cam="             + 
+												  camera,
+										   cam: camera
+										   });
+															   
+							   return video;
+						   }
 		 
-		 var addUpdates = function(){
-				
-				 base.find("img").bind('load', function(){
-				 $(this).attr("src", getSrc());
+		 var addUpdates = function()
+		 {
+			 base.find("img").bind('load', 
+			 	function()
+				{
+			 		$(this).attr("src", getSrc());
 			 
-				 //Adjusting Height and Width
-				 if(!conf.ratio){
-					 //Displaying panoramic video
-					  var result = $(this).width() / $(this).height();
-						if(result < 7 && result > 2)
-						{
-							$(this).attr('width','100%')
-
-							if($(this).height() > base.height())
-							{
-								 $(this).css({'height': base.height() + "px"});
-						 	}
-							
-						 $(this).css("margin-top", ( base.height() - $(this).height() ) / 2+base.scrollTop() + "px");
-						 $(this).css("margin-left", ( base.width() - $(this).width() ) / 2+base.scrollLeft() + "px");
-
-						}else{
-								$(this).css({'width':'100%','height':'100%'});							
-						}
-					 
-
-				 }else{
-					 
-						 if($(this).height() > base.height()){
-							 $(this).css({'height': base.height() + "px"});
-						 }
-						 
-						 if($(this).width() > base.width()){
-							 $(this).css({'width': base.width() + "px"});
-						 }
-						 
-						 $(this).css("margin-top", ( base.height() - $(this).height() ) / 2+base.scrollTop() + "px");
-						 $(this).css("margin-left", ( base.width() - $(this).width() ) / 2+base.scrollLeft() + "px");
+				   //Adjusting Height and Width
+					 if(!conf.ratio)
+					 {
+						 //Displaying panoramic video
 						  var result = $(this).width() / $(this).height();
-					
-						//Displaying panoramic video
-						if(result < 7 && result > 2)
+						  
+							if(result < 7 && result > 2)//Video is panoramic
 							{
-								$(this).attr('width','100%')
+								  $(this).attr('width','100%')
+	  
+								  if($(this).height() > base.height())
+								  {
+									   $(this).css({'height': base.height() + "px"});
+								  }
+								  
+							   $(this).css("margin-top", ( base.height() - $(this).height() ) / 2 + base.scrollTop() + "px");
+							   
+							   $(this).css("margin-left", ( base.width() - $(this).width() ) / 2 + base.scrollLeft() + "px");
+	
+							}else{ //Video is not panoramix
+								
+							   $(this).css({
+											  'width':'100%',
+											  'height':'100%'
+										  });							
 							}
+							
+				   }else{ //Video keeping aspect ration
+					
+					  if($(this).height() > base.height())
+					  {
+					  	 $(this).css({'height': base.height() + "px"});
+				   	  }
+
+					  if($(this).width() > base.width())
+					  {
+						 $(this).css({'width': base.width() + "px"});
+					  }
+						 
+					 $(this).css("margin-top", ( base.height() - $(this).height() ) / 2 + base.scrollTop() + "px");
+					 
+					 $(this).css("margin-left", ( base.width() - $(this).width() ) / 2 + base.scrollLeft() + "px");
+
+					 var result = $(this).width() / $(this).height();
+					
+					//Displaying panoramic video
+					if(result < 7 && result > 2)
+					{
+						$(this).attr('width','100%')
+					}
 				 }
 			 });
 		 }
 		 			 
 		 var loader = $("<img src='"+ settings.url +"/resources/img/3xloader.png'/>")
-		 			   .css({'width':'100%','height':'100%'});
+		 			   .css({
+						   		'width':'100%',
+								'height':'100%'
+							});
 					   
-		 $(base).html(loader).delay(1000).queue(function()
-		 											{
-														$(this).html(createImage()); 
-														addUpdates(); 
-														captionSettings();
-												});
+		 $(base).html(loader)
+		 		.delay(1000)
+				.queue(
+					function()
+					{
+						$(this).html(createImage()); 
+						addUpdates(); 
+						captionSettings();
+					});
 		 
-			return this; 
-	 	}
+		return this; 
+	 }
 	 
-	 $.fn._3xlogic.defaultOptions = {
+	 $.fn._3xcloud.defaultOptions = {
 		 caption:false,
 		 ratio:false,
 		 captionStyle:null,
